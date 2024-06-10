feed206d39f30a2cad1b85d9aee52e14e3a1fbea
Slime <mascarinas022@gmail.com>
2024-06-10 17:05:48 +0800

    add functionality in adding a admin role

--- app/dashboard/members/actions/index.ts
+++ app/dashboard/members/actions/index.ts
@@ -1,9 +1,48 @@
-// "use server";
-export async function createMember() {
-	console.log("create user");
+'use server'
+
+import { createSupabaseAdmin } from '@/lib/supabase'
+
+export async function createMember(data: {
+  name: string
+  role: 'user' | 'admin'
+  status: 'active' | 'resigned'
+  email: string
+  password: string
+  confirm: string
+}) {
+  const supabase = await createSupabaseAdmin()
+  //create account
+  const createResult = await supabase.auth.admin.createUser({
+    email: data.email,
+    password: data.password,
+    email_confirm: true,
+    user_metadata: {
+      role: data.role,
+    },
+  })
+
+  if (createResult.error?.message) {
+    return JSON.stringify(createResult)
+  } else {
+    const memberResult = await supabase
+      .from('member')
+      .insert({ name: data.name, id: createResult.data.user?.id })
+    if (memberResult.error?.message) {
+      return JSON.stringify(memberResult)
+    } else {
+      const permissionReult = await supabase.from('permission').insert({
+        role: data.role,
+        member_id: createResult.data.user?.id,
+        status: data.status,
+      })
+      return JSON.stringify(permissionReult)
+    }
+  }
+  //create member
+  //create permission
 }
 export async function updateMemberById(id: string) {
-	console.log("update member");
+  console.log('update member')
 }
 export async function deleteMemberById(id: string) {}
 export async function readMembers() {}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,10 @@
-import { toast } from "@/components/ui/use-toast";
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { toast } from '@/components/ui/use-toast'
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
+
+export const supabase_url = process.env.NEXT_PUBLIC_SUPABASE_URL
+export const service_role_key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
